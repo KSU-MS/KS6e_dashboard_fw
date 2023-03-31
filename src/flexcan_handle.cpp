4dfe4e71b71d35b394be27a19a36e6cc5060e6ee
jstri114 <jstri114@students.kennesaw.edu>
2023-03-31 19:09:04 -0400

    Reverted to 125kps

--- src/flexcan_handle.cpp
+++ src/flexcan_handle.cpp
@@ -13,7 +13,7 @@ FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> Inverter_CAN_;
 void init_can(){
 //inverter can must send & receive, 6rx MB and 2tx MB
 Inverter_CAN_.begin();
-Inverter_CAN_.setBaudRate(125000);
+Inverter_CAN_.setBaudRate(500000);
 Inverter_CAN_.setMaxMB(NUM_TX_MAILBOXES + NUM_RX_MAILBOXES);
 for (int i = 0; i < NUM_RX_MAILBOXES; i++)
 {
